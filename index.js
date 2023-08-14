48ea2791c84a535ad1749234ce96f41bc2b01a9d
x1yl <life23243@tutanota.com>
2023-08-14 23:12:29 +0000

    Formatted all files and fixed copying calculation.

--- index.js
+++ index.js
@@ -7,24 +7,21 @@ function calculator(option) {
     calculation = eval(calculation);
   } else if (option === "clear") {
     calculation = "";
-  } else if (option === "copy") {
+  } else if (option === "calculation") {
     navigator.clipboard.writeText(calculation);
-  } else if (option === "theme") {
-    // Handle theme switching logic here
-    // You can modify the CSS href based on the selected theme
   } else {
     calculation += option;
   }
   calculationSpan.innerHTML = calculation;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll("button").forEach((button) => {
-    button.addEventListener("click", function () {
+    button.addEventListener("click", function() {
       calculator(button.id);
     });
   });
-  
+
   const dropdown = document.getElementById('dropdown');
   dropdown.addEventListener('change', function() {
     css.href = "themes/" + dropdown.value + ".css";
