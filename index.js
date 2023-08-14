985e13607f416c7be698e2803e4fc10064963c67
x1yl <life23243@tutanota.com>
2023-08-14 18:47:24 +0000

    Moved css files into a new folder called themes for clarity

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
   
   const dropdown = document.getElementById('dropdown');
   dropdown.addEventListener('change', function() {
-    css.href = dropdown.value + ".css";
+    css.href = "themes/" + dropdown.value + ".css";
   });
 });
